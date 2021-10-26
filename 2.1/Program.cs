fcf91a365cd97e6fe820834b9cf29a57f7e84f88
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2021-10-26 20:03:41 +0200

    Добавьте файлы проекта.

---
+++ 2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.1. 2-й вариант, высокий уровень, Гребенюк.
+            Console.WriteLine("Введите значение x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение y: ");
+            int y = int.Parse(Console.ReadLine());
+            double r = Math.Sqrt(x * x + y * y);
+            if (r <= 1)
+                Console.WriteLine("Точка не попадает в область определения.");
+            else if ((x <= 1) && (x >= -1) && (y <= 1) && (y >= -1))
+                Console.WriteLine("Точка попадает в область определения.");
+            else
+                Console.WriteLine("Точка не попадает в область определения.");
+        }
+    }
+}
